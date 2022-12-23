4cba34692bde2655ae5ca312d31f8056675d19fa
eliashead <eliashead.costa@gmail.com>
2022-12-23 19:10:57 -0300

    feat: add home slider mobile

--- src/components/Slider/styles.ts
+++ src/components/Slider/styles.ts
@@ -3,28 +3,32 @@ import { styled } from '../../styles'
 import { Swiper, SwiperSlide } from 'swiper/react'
 
 export const SwiperS = styled(Swiper, {
-  width: '80%',
-  height: '60vh',
+  width: '100%',
+  height: '40vh',
   margin: '0 auto',
 
   '@bp4': {
     maxWidth: '1440px',
+    width: '80%',
   },
 })
 
 export const SwiperSlideS = styled(SwiperSlide, {
-  width: '90%',
-
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
 
   img: {
     objectFit: 'cover',
-    borderRadius: '0.8rem',
+    width: 'inherit',
+    height: '100%',
+    padding: '1rem',
+    borderRadius: '2rem',
   },
 
   '@bp4': {
+    width: '100%',
+    height: '100%',
     margin: '0 auto',
     padding: '1rem',
 
